02e89831fb751a6e5c98f0ab17a1b78287f0b375
st091150 <72318993+st091150@users.noreply.github.com>
2020-10-04 00:32:29 +0430

    Add files via upload

---
+++ snail.cpp
@@ -0,0 +1,40 @@
+﻿// snail.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    do { std::cout << "Rows: "; std::cin >> a; } while (a < 1);
+    do { std::cout << "Columns: "; std::cin >> b; } while (b < 1);
+
+    int** M = new int* [a];
+    for (int i = 0; i < a; ++i)
+        M[i] = new int[b];
+
+    int p = 0;
+
+    for (int k = 0; k <= a - 1; ++k) {
+        for (int j = 0; j <= b - 1; ++j) {
+            M[k][j] = p;
+            ++p;
+        }
+        std::cout << std::endl;
+    }
+
+    for (int k = 0; k <= a - 1; k++) {
+        for (int j = 0; j <= b - 1; j++) {
+            std::cout << M[k][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < a; ++i)
+        delete[] M[i];
+
+    delete M;
+
+}
+
+
